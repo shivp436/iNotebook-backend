0442d9ade06f8ec37d11ff330e2eedf48c0016e8
shivp436 <mpshivanandpatil2002@gmail.com>
2024-09-17 03:19:38 +0530

    Init

---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import colors from 'colors';  // Import colors library
+
+const connectDB = async (): Promise<void> => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI is not defined in environment variables');
+    }
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(colors.cyan.underline(`MongoDB Connected: ${conn.connection.host}`));
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(colors.red(`Error: ${error.message}`));
+    } else {
+      console.error(colors.red('Unknown error occurred'));
+    }
+    process.exit(1);
+  }
+}
+
+export { connectDB };
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const port = Number(
+	process.env.NODE_ENV === 'development'
+		? process.env.DEV_PORT || 3000
+		: process.env.PROD_PORT || 5000
+);
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
